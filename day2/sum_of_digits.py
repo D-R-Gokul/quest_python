092b7bd8e3702884a705894af8934adabcc7b6b7
D-R-Gokul <gokulachu005@gmail.com>
2024-07-31 17:25:52 +0530

    updated

--- day2/sum_of_digits.py
+++ day2/sum_of_digits.py
@@ -1,10 +1,10 @@
 #program to find the sum of digits in the number
 
 input_number=int(input("Enter a number to count the number of odd numbers in it: "))
-sum_of_digits=0
-temp_number= input_number
+sum_of_digits = 0
+temp_number = input_number
 while(temp_number>0):
-    r=temp_number%10
+    r = temp_number %10
     sum_of_digits += r
     temp_number=temp_number//10
     
