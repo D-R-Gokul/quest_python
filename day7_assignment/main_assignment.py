077cf6d0f5e88ce04e587ea77eb3d52d062fe547
D-R-Gokul <gokulachu005@gmail.com>
2024-08-07 11:06:44 +0530

    updated assignment

--- day7_assignment/main_assignment.py
+++ day7_assignment/main_assignment.py
@@ -31,31 +31,29 @@
 
 
 
-name = input("Please enter your name: ")
+#name = input("Please enter your name: ")
 age = int(input("Enter age: "))
-gender = input("Enter gender: \nM. Male\nF. Female\nO. Other\nEnter any number.")
+gender = input("Enter gender: \nM. Male\nF. Female: \nEnter any number.")
 occupation = int(input("Select your occupation \n1: Student \n2: Working"))
 is_hosteler = int(input("Enter \n1.hosteler \n2.non-hosteler"))
-if is_hosteler == 1:
-    hostel_name= input("Enter the hostel name: ")
-
-
 
 if age >= 60 and gender.lower() == "m":
-    print("Senior citizen discount applied, thank you for shopping")
+    print("Senior citizen discount 15 percent applied, thank you for shopping")
 elif age >= 45 and gender.lower() == "f":
-    print("Senior citizen discount applied, thank you for shopping")
-    
-elif is_hosteler == 1 and occupation ==1:
-    print("Offers on Groceries")
-elif age <60 and occupation == 1  and is_hosteler == 2:
-    print("500 coupon on books, Thankyou")
-
-elif age <60 and gender.lower() == 'm':
-    print("You have received Fastrack voucher worth of Rs:100 ")
-elif age < 45 and gender.lower() == 'f':
-    print("You have received Nyka voucher worth of Rs:100")
-
+    print("Senior citizen discount 15 percent applied, thank you for shopping")
 else:
-    print("Thank you for shopping")
-	
+    if age <60 and gender== 1:
+        print("You have received Fastrack voucher worth of Rs:100 ")
+    else: 
+        print("You have received Nyka voucher worth of Rs:100")
+
+if age < 45:
+    if occupation == 1 and is_hosteler == 2:
+        print("You have won a  Rs 500 coupon on books")
+    
+    elif is_hosteler == 1 and occupation ==1:
+        print("Offers on Groceries and Students coupon of Rs:500 on books applied")
+    elif occupation == 2  and is_hosteler == 2:
+        print("discount on Groceries, Thankyou")
+    else:
+        print("Thank you for shopping")
